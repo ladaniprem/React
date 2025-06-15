2b7fa957e4440196cc4f13446dee4bb61ebd8f67
Ladani prem <premladani33453@gmail.com>
2025-06-15 22:11:19 +0530

    feat: Update Github component to use loader data and enhance routing

--- 07-React-Router/src/components/Github/Github.jsx
+++ 07-React-Router/src/components/Github/Github.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+// import { useEffect } from 'react';
+// import { useState } from 'react';
+ import { useLoderData } from 'react-router-dom';
 
 function Github() {
+    const data = useLoderData()
+    // const [data,setData] = useState([])
+    // useEffect(()=> {
+    //     fetch('https://api.github.com/users/ladaniprem')
+    //     .then((res) => res.json())
+    //     .then(data=>{
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // },[])
   return (
-    <div>
-      
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Git picture" width={300} />
     </div>
   )
-}
-
+} 
 export default Github
+// export const githubInfoLoader = async () => {
+//     const res = await fetch('https://api.github.com/users/ladaniprem')
+//     return res.json()
+// }
--- 07-React-Router/src/main.jsx
+++ 07-React-Router/src/main.jsx
@@ -1,30 +1,54 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from "react-router/dom"
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom"
 import Layout from './Layout'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 import './index.css'
 
-const router = createBrowserRouter([
-  {
-     path : '/',
-     element : <Layout />,
-     children:[
-      {
-         path: "Home",
-         element: <Home />
-      },
-      {
-         path: "about",
-         element: <About />
-      }
-     ]
-  }
-])
+const githubInfoLoader = async () => {
+  const response = await fetch('https://api.github.com/users/ladaniprem')
+  return response.json()
+}
 
+// const router = createBrowserRouter([
+//   {
+//      path : '/',
+//      element : <Layout />,
+//      children:[
+//       {
+//          path: "",
+//          element: <Home />
+//       },
+//       {
+//          path: "about",
+//          element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//      ]
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route 
+        path='github' 
+        element={<Github />}
+        loader={githubInfoLoader}
+      />
+    </Route>
+  )
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 )
